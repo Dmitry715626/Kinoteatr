1284e6603c60356029fcd26f14123817b7a8b5ea
Dmitry715626 <60756760+Dmitry715626@users.noreply.github.com>
2023-01-27 21:01:05 +0300

    Добавьте файлы проекта.

---
+++ Kinoteatr/Films.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinoteatr
+{
+    public class Films
+    {
+        public string name { get; set; }
+        public List<Sessions> SessionSource { get; set; }
+        public Films()
+        {
+            SessionSource = new List<Sessions>();
+
+            SessionSource.Add(new Sessions() { SessionTime = "11:30" });
+            SessionSource.Add(new Sessions() { SessionTime = "14:25" });
+            SessionSource.Add(new Sessions() { SessionTime = "18:00" });
+            SessionSource.Add(new Sessions() { SessionTime = "21:45" });
+            name = "Классное название фильма";
+        }
+    }
+}
---
+++ Kinoteatr/Sessions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinoteatr
+{
+    public class Sessions
+    {
+        public string SessionTime { get; set; }
+        public string SessionPrice { get; set; }
+    }
+}
